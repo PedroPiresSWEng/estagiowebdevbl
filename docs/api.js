1079d68406e9e4e752e5bf6af77dd60b9208d142
Pedro Pires <pedrinxx1@gmail.com>
2018-03-17 21:53:21 -0300

    Create api.js

---
+++ docs/api.js
@@ -0,0 +1,14 @@
+{
+   "destination_addresses" : [ "" ],
+   "origin_addresses" : [ "" ],
+   "rows" : [
+      {
+         "elements" : [
+            {
+               "status" : "NOT_FOUND"
+            }
+         ]
+      }
+   ],
+   "status" : "OK"
+}
